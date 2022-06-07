87c4fa80f7a4526577d29e0c82bf0dd650a70ea2
bg-dev <nik.azizoff2016@gmail.com>
2022-06-07 14:01:55 +0300

    fix field "login" to "username"

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,46 +1,45 @@
-const express = require("express"); 
-const { addUser, loginUser } = require('../../service/userService')
-const logger = require('../middlewares/logger')
+const express = require("express");
+const { addUser, loginUser } = require("../../service/userService");
+const logger = require("../middlewares/logger");
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/register', async(req, res, next) => {
-    try {
-        const {login, password} = req.body
-        const user = {
-            login,
-            password
-        }
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = {
+      username,
+      email,
+      password,
+    };
 
-        await addUser(user)
-        const message = `User ${user.login} was created`
-        logger.info(message)
-        res.status(201).send({ message })
-    } catch (error) {
-        next(error)
-    }
-})
+    await addUser(user);
+    const message = `User ${user.username} was created`;
+    logger.info(message);
+    res.status(201).send({ message });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/login', async(req, res, next) => {
-    try {
-        const {login, password} = req.body
-        const user = {
-            login,
-            password
-        }
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = {
+      username,
+      password,
+    };
 
-        const loginData = await loginUser(user)
-        const message = `User ${user.login} was logged in`
-        logger.info(message)
-        res.status(201).send({
-            ...loginData,
-            message
-        })
-    } catch (error) {
-        next(error)
-    }
-})
+    const loginData = await loginUser(user);
+    const message = `User ${user.username} was logged in`;
+    logger.info(message);
+    res.status(201).send({
+      ...loginData,
+      message,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-
-
-module.exports = router
+module.exports = router;
--- service/userService.js
+++ service/userService.js
@@ -1,67 +1,60 @@
-const { validateUser } = require('./validators/userValidator')
-const { writeDataToJsonFile } = require('./commandHelper')
-const users = require('../integration/databases/users.json')
-const bcrypt = require('bcrypt')
-const uuid = require('uuid');
-const jwt = require('jsonwebtoken');
-const { getElementFromArrayById } = require('./arrayService');
+const { validateUser, validateNewUser } = require("./validators/userValidator");
+const { writeDataToJsonFile } = require("./commandHelper");
+const users = require("../integration/databases/users.json");
+const bcrypt = require("bcrypt");
+const uuid = require("uuid");
+const jwt = require("jsonwebtoken");
+const { getElementFromArrayById } = require("./arrayService");
 
-const USERS_FILE = 'users.json'
+const USERS_FILE = "users.json";
 
-async function addUser(newUserData){
-    const { error } = validateUser(newUserData)
-    if(error)
-        throw new Error(error.details[0].message)
+async function addUser(newUserData) {
+  const { error } = validateNewUser(newUserData);
+  if (error) throw new Error(error.details[0].message);
 
-    const candidate = findUser(newUserData)
-    if(candidate)
-        throw new Error("This user already exists")
+  const candidate = findUser(newUserData);
+  if (candidate) throw new Error("This user already exists");
 
-    const hashedPassword = await bcrypt.hash(newUserData.password, 12)
-    const newUser = {
-        id: uuid.v4(),
-        login: newUserData.login,
-        password: hashedPassword
-    }
-    
-    const newUsers = [...users, newUser]
-    writeDataToJsonFile(newUsers, USERS_FILE)
+  const hashedPassword = await bcrypt.hash(newUserData.password, 12);
+  const newUser = {
+    id: uuid.v4(),
+    username: newUserData.username,
+    email: newUserData.email,
+    password: hashedPassword,
+  };
+
+  const newUsers = [...users, newUser];
+  writeDataToJsonFile(newUsers, USERS_FILE);
 }
 
-async function loginUser(userData){
-    const { error } = validateUser(userData)
-    if(error)
-        throw new Error(error.details[0].message)
+async function loginUser(userData) {
+  const { error } = validateUser(userData);
+  if (error) throw new Error(error.details[0].message);
 
-    const user = findUser(userData)
+  const user = findUser(userData);
 
-    if(!user)
-        throw new Error("This user no exists")
+  if (!user) throw new Error("This user no exists");
 
-    const isMatch = await bcrypt.compare(userData.password, user.password)
+  const isMatch = await bcrypt.compare(userData.password, user.password);
 
-    if(!isMatch)
-        throw new Error("Password is incorrect")
-    
-    const token = jwt.sign(
-        { userId: user.id },
-        process.env.JWT_SECRET,
-        { expiresIn: '1h' }
-    )
+  if (!isMatch) throw new Error("Password is incorrect");
 
-    return {token, login: userData.login, userId: user.id}
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+
+  return { token, username: userData.login, userId: user.id };
 }
 
+const findUser = (user) =>
+  users.find(
+    (elem) => elem.username.toLowerCase() === user.username.toLowerCase()
+  );
 
-const findUser = user => 
-    users
-        .find(elem => elem.login.toLowerCase() === user.login.toLowerCase())
-
-const findUserById = userId =>
-    getElementFromArrayById(users, userId)
+const findUserById = (userId) => getElementFromArrayById(users, userId);
 
 module.exports = {
-    addUser,
-    loginUser,
-    findUserById
-}
+  addUser,
+  loginUser,
+  findUserById,
+};
